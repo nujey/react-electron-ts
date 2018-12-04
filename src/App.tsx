d0694843baef9ea2f330772020ea31d9bc6e9999
张锋 <zhangfeng930101@qq.com>
2018-12-04 17:13:55 +0800

    typescript home

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './styles/App.css';
 
 import logo from './assets/images/logo.svg';
+// import Hello from './views/home/home';
 
 class App extends React.Component {
   public render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        {/* <Hello name="张小鹿" enthusiasmLevel={3}/> */}
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/views/home/home.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+}
+
+function Hello({ name, enthusiasmLevel = 1 }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiasmLevel. :D');
+  }
+  return (
+    <div className="hello">
+      <div>
+        Hello!{name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+    </div>
+  )
+}
+// class Hello extends React.Component<Props, object> {
+//   constructor(props: Props) {
+//     super(props)
+//   }
+//   render() {
+//     const { name, enthusiasmLevel = 1} = this.props;
+//     return (
+//       <div className="hello">
+//         Hello!{name + getExclamationMarks(enthusiasmLevel)}
+//       </div>
+//     )
+//   }
+// }
+
+export default Hello
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!')
+}
