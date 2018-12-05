52bd6ed91a80ce195ebd252f4b34d2d37fb8ff86
张锋 <zhangfeng930101@qq.com>
2018-12-05 15:49:18 +0800

    react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import * as _ from 'lodash';
+
 import App from './App';
 import './styles/index.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+  , document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
