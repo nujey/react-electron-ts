31e34104dee78e50e327c977e32c2a06728dcf9b
张锋 <zhangfeng930101@qq.com>
2018-12-20 19:29:19 +0800

    s

--- src/views/home/usercenter.tsx
+++ src/views/home/usercenter.tsx
@@ -5,15 +5,18 @@ interface Props {
   username: string
 }
 const Component = (props: Props & RouteComponentProps<{}>) => <h2>Welcome! {props.username}</h2>
-const WithRouterComponent = withRouter<Props>(Component)
 
-class UserCenter extends React.Component<Props> {
-  public render() {
-    console.log(this.props)
-    return(
-      <div>个人中心{this.props.username}</div>
-    )
-  }
-}
+const WithRouterComponent = withRouter<Props>(Component);
+
+const UserCenter = () => (<WithRouterComponent username="测试的"/>)
+
+// class UserCenter extends React.Component<Props> {
+//   public render() {
+//     console.log(this.props)
+//     return(
+//       <div>个人中心{this.props.username}</div>
+//     )
+//   }
+// }
 
 export default UserCenter
