c39cf0ae62093b099b9a17c8f91aa79097d99af1
张锋 <zhangfeng930101@qq.com>
2018-12-24 09:33:13 +0800

    s

--- src/views/home/usercenter.tsx
+++ src/views/home/usercenter.tsx
@@ -1,22 +1,25 @@
 import * as React from 'react'
-import { withRouter, RouteComponentProps } from 'react-router-dom'
+// import { withRouter, RouteComponentProps } from 'react-router-dom'
 
 interface Props {
   username: string
 }
-const Component = (props: Props & RouteComponentProps<{}>) => <h2>Welcome! {props.username}</h2>
+// const Component = (props: Props & RouteComponentProps<{}>) => <h2>Welcome! {props.username}</h2>
 
-const WithRouterComponent = withRouter<Props>(Component);
+// const WithRouterComponent = withRouter<Props>(Component);
 
-const UserCenter = () => (<WithRouterComponent username="测试的"/>)
-
-// class UserCenter extends React.Component<Props> {
-//   public render() {
-//     console.log(this.props)
-//     return(
-//       <div>个人中心{this.props.username}</div>
-//     )
-//   }
-// }
+// const UserCenter = () => (<WithRouterComponent username="测试的"/>)
+interface State {
+  name: ''
+}
+class UserCenter extends React.Component<Props, State> {
+  readonly state = {} as State
+  public render() {
+    console.log(this.state)
+    return(
+      <div>个人中心{this.props.username} {this.state.name}</div>
+    )
+  }
+}
 
 export default UserCenter
